35803f84b56339ecb76fcecc22ea97cf08090afb
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-23 09:53:26 -0300

    feat(domain): add get-character use case

---
+++ src/@core/domain/usecases/get-character.ts
@@ -0,0 +1,17 @@
+import { Character } from '@domain/entities'
+
+export interface GetCharacterUseCase {
+  get: (
+    params: GetCharacterUseCase.Request,
+  ) => Promise<GetCharacterUseCase.Response>
+}
+
+export namespace GetCharacterUseCase {
+  export type Request = {
+    id: string
+  }
+
+  export type Response = {
+    character: Character
+  }
+}
