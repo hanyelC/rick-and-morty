b7c8b8cd706a992decb8b54c35f1d60fb6910a20
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-23 14:49:41 -0300

    feat: initial Home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,45 @@
+import styles from '../styles/pages/home.module.css'
+import { Card } from '@/components/Card'
+import { FetchCharactersUseCase } from '@application/usecases'
+import { Character } from '@domain/entities'
+import { FakeCharacterGateway } from '@infra/gateways'
+
+import { GetStaticProps, NextPage } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return <h1 className={inter.className}>Lorem ipsum</h1>
+type Props = {
+  characters: Character[]
 }
+
+const Home: NextPage<Props> = ({ characters }) => {
+  return (
+    <div className={`${styles.container} ${inter.className}`}>
+      <main>
+        <h1 className={inter.className}>Lorem ipsum</h1>
+        <div>
+          {characters.map((item) => (
+            <Link key={item.id} href={`/character/${item.id}`}>
+              <Card name={item.name} />
+            </Link>
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const gateway = new FakeCharacterGateway()
+  const useCase = new FetchCharactersUseCase(gateway)
+
+  const { characters } = await useCase.fetch()
+
+  return {
+    props: { characters },
+  }
+}
+
+export default Home
