31a6fcafeece8d7bd6d85f9cf052186a41728bf4
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-23 10:20:39 -0300

    feat(application/contracts): add GetCharacterGateway

---
+++ src/@core/application/contracts/get-character.gateway.ts
@@ -0,0 +1,17 @@
+import { Character } from '@domain/entities'
+
+export interface GetCharacterGateway {
+  getById: (
+    params: GetCharacterGateway.Request,
+  ) => Promise<GetCharacterGateway.Response>
+}
+
+export namespace GetCharacterGateway {
+  export type Request = {
+    id: string
+  }
+
+  export type Response = {
+    character: Character
+  }
+}
