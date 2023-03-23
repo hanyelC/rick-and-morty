fe334f72a58fa3cdaa2cf3ae743ffd8162a78c2e
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-23 14:45:20 -0300

    feat: add global styles to App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
+import '../styles/globals.css'
+import styles from '../styles/pages/app.module.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+      <header>Rick and Morty</header>
+      <Component {...pageProps} />
+    </div>
+  )
 }
