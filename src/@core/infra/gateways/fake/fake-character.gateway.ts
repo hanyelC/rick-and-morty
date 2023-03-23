a5f676287720d292b28822eafb31917be0cf579c
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-23 14:16:28 -0300

    feat(infra): implement FetchCharactersGateway on fake gateway

--- src/@core/infra/gateways/fake/fake-character.gateway.ts
+++ src/@core/infra/gateways/fake/fake-character.gateway.ts
@@ -1,7 +1,18 @@
-import { GetCharacterGateway } from '@application/contracts'
+import {
+  FetchCharactersGateway,
+  GetCharacterGateway,
+} from '@application/contracts'
 import { characters } from '@infra/data-sources/character'
 
-export class FakeCharacterGateway implements GetCharacterGateway {
+export class FakeCharacterGateway
+  implements GetCharacterGateway, FetchCharactersGateway
+{
+  async fetchAll(
+    params?: FetchCharactersGateway.Request,
+  ): Promise<FetchCharactersGateway.Response> {
+    return { characters }
+  }
+
   async getById({
     id,
   }: GetCharacterGateway.Request): Promise<GetCharacterGateway.Response> {
