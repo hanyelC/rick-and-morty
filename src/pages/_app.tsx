8c216c72510f25d565c108c37dc0443ae86c19e3
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-25 11:04:39 -0300

    feat: add Link to home page on header and add styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import '../styles/globals.css'
 import styles from '../styles/pages/app.module.css'
 
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={styles.container}>
-      <header>Rick and Morty</header>
+      <header className={styles.header}>
+        <div>
+          <Link href="/">Rick and Morty</Link>
+        </div>
+      </header>
       <Component {...pageProps} />
     </div>
   )
